500414fbf120c8d6fbea6adbb869e198e6ed6956
Ashley Etemadi <ash.etemadi@gmail.com>
2020-04-07 00:53:04 +0000

    Automatically backed up by Learn

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,3 +1,35 @@
 class FundingRound
+    attr_reader :startup, :vc, :type, :investment  
+
+    @@all = []
+
+    def initialize(startup, vc, type, investment)
+        @startup = startup 
+        @vc = vc 
+        self.type = type 
+        self.investment = investment.to_f
+        self.class.all << self 
+    end 
+
+    def type=(type)
+        if type.class != String 
+            raise ArgumentError, "Funding type must be a string."
+        else 
+            @type = type 
+        end 
+    end 
+
+    def investment=(investment)
+        if investment > 0
+            @investment = investment
+        else 
+            nil  
+        end 
+    end 
+
+    def self.all 
+        @@all 
+    end 
+
 
 end
